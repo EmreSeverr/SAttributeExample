4efaf4cc42a9b8e4dd2b139eab69ac08bb24375b
Emre Sever <emresever@milvasoft.com>
2020-12-10 01:59:30 +0300

    update

--- SAttributeExample/Controllers/ProductsController.cs
+++ SAttributeExample/Controllers/ProductsController.cs
@@ -24,7 +24,19 @@ namespace SAttributeExample.Controllers
         [HttpGet("Product")]
         public async Task<IActionResult> GetAllProduct()
         {
-            return Ok(await _productRepository.GetAllAsync().ConfigureAwait(false));
+            var product = await _productRepository.GetAllAsync().ConfigureAwait(false);
+
+            var productDTO = product != null ?
+                             (from p in product
+                              select new ProductDTO
+                              {
+                                  Id = p.Id,
+                                  Barcode = p.Barcode,
+                                  Name = p.Name,
+                                  UnitPrice = p.UnitPrice
+                              }).ToList() : null;
+
+            return Ok(productDTO);
         }
 
         [HttpPost("Product")]
